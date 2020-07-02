25db1b6541a7d0f566121f3a73746b7c1f5637c3
Paul Winfield <paul@Pauls-MacBook-Pro.local>
2020-07-02 16:52:38 +0100

    Added parent category to category interface

--- src/interfaces/category.ts
+++ src/interfaces/category.ts
@@ -2,7 +2,8 @@ import { IBreadcrumbPathEntry } from './breadcrumb-path-entry';
 import { IKeyedItem } from "./keyed-item";
 
 export interface ICategory extends IKeyedItem {
-    parentId: number;
+    parentId: number,
+    parentCategory: ICategory,
     description: string,
     documentUrl: string,
     path?: Array<IBreadcrumbPathEntry>,
