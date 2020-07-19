45c30be9d633930bc2c3ae10d340d34004099da9
Paul Winfield <paul@Pauls-MacBook-Pro.local>
2020-07-19 10:32:49 +0100

    Added booking priority typings

--- src/interfaces/booking-priority.ts
+++ src/interfaces/booking-priority.ts
@@ -4,3 +4,37 @@ export enum BookingPriority {
     p3,
     p4
 }
+
+export interface IBookingPriorityEntry {
+    bookingPriority: BookingPriority,
+    name: String,
+    description: string,
+    icon?: string,
+}
+
+export const bookingPriorities: Array<IBookingPriorityEntry> = [
+    { 
+        bookingPriority : BookingPriority.p1, 
+        name: "P1",
+        description: "Same day",
+        icon: "notification_important"
+    },
+    { 
+        bookingPriority : BookingPriority.p2, 
+        name: "P2",
+        description: "Next working day",
+        icon: "access_alarm"
+    },
+    { 
+        bookingPriority : BookingPriority.p3, 
+        name: "P3",
+        description: "Within 48 hours",
+        icon: "access_time"
+    },
+    { 
+        bookingPriority : BookingPriority.p4, 
+        name: "P4",
+        description: "Within 7 days",
+        icon: "date_range"
+    }
+]
