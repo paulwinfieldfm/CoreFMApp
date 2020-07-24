9bf37955014f160c8608a7c13aca7ef2b92d4f87
Paul Winfield <paul@Pauls-MacBook-Pro.local>
2020-07-24 07:49:26 +0100

    Added asset parent

--- src/interfaces/asset.ts
+++ src/interfaces/asset.ts
@@ -3,6 +3,7 @@ import { IKeyedItem } from "./keyed-item";
 export interface ICoreAsset extends IKeyedItem {
     code: String,
     coordinates?: String,
+    parentId?: number,
 }
 
 export interface IAsset extends ICoreAsset {
