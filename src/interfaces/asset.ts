58e455e41f712b5dd5d9f492cd10d89a081f3ad6
Paul Winfield <paul@Pauls-MacBook-Pro.local>
2020-07-24 10:03:18 +0100

    Added asset node descriptor

--- src/interfaces/asset.ts
+++ src/interfaces/asset.ts
@@ -9,3 +9,10 @@ export interface ICoreAsset extends IKeyedItem {
 export interface IAsset extends ICoreAsset {
     assets?: Array<IAsset>,
 }
+
+export interface IAssetNode {
+    id: number,
+    name: string,
+    isLocation: boolean,
+    children?: IAssetNode[]
+}
