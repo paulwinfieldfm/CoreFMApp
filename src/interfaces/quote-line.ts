03cf28c7a461057b0ebd014b10528b3f4f396957
Paul Winfield <paul@Pauls-MacBook-Pro.local>
2020-08-11 10:15:08 +0100

    Added interfaces for quote response

---
+++ src/interfaces/quote-line.ts
@@ -0,0 +1,15 @@
+export enum QuoteLineCategory {
+    time,
+    materials,
+}
+
+export interface IQuoteLine {
+    title: string,
+    description?: string,
+    quoteLineCategory: QuoteLineCategory,
+    quantity?: number,
+    price?: number,
+    linePrice?: number,
+    taxRate: number,
+}
+  
---
+++ src/interfaces/quote-response.ts
@@ -0,0 +1,14 @@
+import { IAuditedItem } from "./audited-item";
+import { ISupplier } from "./supplier";
+import { IItemAttributeMap } from "./item-attribute";
+import { IQuoteLine } from "./quote-line";
+
+export interface IQuoteResponse extends IAuditedItem {
+    quoteReference: string,
+    supplier: ISupplier,
+    properties: IItemAttributeMap,
+    quoteLines: Array<IQuoteLine>,
+    anticipatedVisitCount?: number,
+    siteSurveyRequired: boolean,
+    soonestServiceDate: number,
+}
