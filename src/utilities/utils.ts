6e5fde453ba2d2afbc6afd5d6e7248c6878fe903
Paul Winfield <paul@Pauls-MacBook-Pro.local>
2020-09-14 16:10:05 +0100

    Added isObject check

--- src/utilities/utils.ts
+++ src/utilities/utils.ts
@@ -66,6 +66,10 @@ export class Utils {
     }
     return result.toUpperCase();
   }
+  static isObject(value: any): boolean {
+    if (value === null) { return false;}
+    return ( (typeof value === 'function') || (typeof value === 'object') );      
+  }
   static async asyncForEach(array: Array<any>, callback: any) {
     for (let index = 0; index < array.length; index++) {
       await callback(array[index], index, array);
