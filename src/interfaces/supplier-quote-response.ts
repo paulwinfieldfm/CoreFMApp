11f846b3e5445e11f159bf3811bf3604675ff900
Paul Winfield <paul@Pauls-MacBook-Pro.local>
2020-09-16 10:51:47 +0100

    Added quote response status

--- src/interfaces/supplier-quote-response.ts
+++ src/interfaces/supplier-quote-response.ts
@@ -6,8 +6,18 @@ import { QuoteResponseArea } from "./quote-response-status";
 export interface IQuoteResponseStatus extends IActionStatus {
     quoteArea: QuoteResponseArea;
 }
+
+export enum QuoteResponseStatus {
+    unknown,
+    reviewing,
+    pending,
+    moreInformationRequired,
+    declined,
+    quoteProvided,
+}
  
 export interface ISupplierQuoteResponse extends IServiceRequest {
+    quoteResponseStatus: QuoteResponseStatus,
     supplier: ISupplier,
     progress: Array<IQuoteResponseStatus>,
     confirmedDateTime?: number,
