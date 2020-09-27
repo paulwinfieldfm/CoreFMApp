67e30a069f27acce4b191a2331e71a6c4e73f36e
Paul Winfield <paul@Pauls-MacBook-Pro.local>
2020-09-27 10:30:40 +0100

    Revised supplier quote response modelling

--- src/interfaces/supplier-quote-response.ts
+++ src/interfaces/supplier-quote-response.ts
@@ -2,11 +2,19 @@ import { ISupplier } from "./supplier";
 import { IActionStatus } from "./action-status";
 import { IServiceRequest } from "./service-request";
 import { QuoteResponseArea } from "./quote-response-status";
+import { IItemAttributeMap, IPriceLine } from ".";
 
 export interface IQuoteResponseStatus extends IActionStatus {
     quoteArea: QuoteResponseArea;
 }
 
+export enum QuoteResponseProvided {
+    pending,
+    declined,
+    quoteGiven,
+}
+/*
+Deprecated?
 export enum QuoteResponseStatus {
     unknown,
     reviewing,
@@ -19,10 +27,37 @@ export enum QuoteResponseStatus {
     declined,
     quoteProvided,
 }
- 
+*/
+/*
+export enum QuoteResponseArea {
+    reviewQuote,
+    declinedToQuote,
+    specifyAvailability,
+    specifyCostings,
+    specifyProperties,
+    confirmation,
+    refineQuote
+}
+*/
 export interface ISupplierQuoteResponse extends IServiceRequest {
-    quoteResponseStatus: QuoteResponseStatus,
+    // Can we/have we quoted?
+    quoteResponseProvided: QuoteResponseProvided,
     supplier: ISupplier,
+    // Indicate how far through we've got for the llinear progress tracker
     progress: Array<IQuoteResponseStatus>,
+    // Confirm providing our initial response to the quote request
     confirmedDateTime?: number,
+    // Quote will be closed off when there's no further dialog or if it was cancelled etc
+    closedDateTime?: number,
+    openedDateTime?: number,
+    haveOpenQuestions: boolean,
+    // Custom properties can be specified by the supplier
+    properties: IItemAttributeMap,
+    // Pricing
+    quoteLines: Array<IPriceLine>,
+    anticipatedVisitCount?: number,
+    siteSurveyRequired: boolean,
+    soonestServiceDate: number,
+    bestAndFinalOffer: boolean,
+    declineToQuoteReason?: string,
 }
