2dab32dcde005a3b3e466459298d4aca957dd20a
Paul Winfield <paul@Pauls-MacBook-Pro.local>
2020-09-29 17:47:40 +0100

    Add soem additional assignment properties to the job object

--- src/interfaces/job.ts
+++ src/interfaces/job.ts
@@ -1,6 +1,7 @@
 import { IAuditedItem, IService, BookingPriority, IContact, ICompany, ILocation, IAsset, IPlottableLocation } from ".";
 import { IOrder } from "./order";
 import { IKeyedPersonItem, IKeyedImageItem } from "./keyed-item";
+import { IQuoteRequest } from "./quote-request";
 
 export enum JobStatus {
     new,
@@ -28,7 +29,15 @@ export interface IJobStatusEvent extends IPlottableLocation {
 
 export interface IJob extends IAuditedItem {
     reference: string,
-    order: IOrder,
+    requiredDateTime?: number,
+    bookingDateTime?: number,
+    bookingDurationMinutes?: number,
+    estimatedTravelTimeMinutes?: number,
+    // We can have a pre-emptive assignment that's not associated with a work order
+    order?: IOrder,
+    // Or have a non-binding placeholder assignment to a quote to give the ability to
+    // ringfence a booking in expectation of it dropping in
+    quotePlaceholder?: IQuoteRequest,
     assignedToUser?: IKeyedPersonItem,
     statusHistory: Array<IJobStatusEvent>,
     currentStatus: JobStatus,
