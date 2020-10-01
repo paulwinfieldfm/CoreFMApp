5ee3d3e1963d61cc58a8db530941990e82407e07
Paul Winfield <paul@Pauls-MacBook-Pro.local>
2020-10-01 20:14:04 +0100

    Added KPI vide model

--- src/interfaces/view-models.ts
+++ src/interfaces/view-models.ts
@@ -3,6 +3,18 @@ import { IItemAttributeMap } from "./item-attribute";
 import { IKeyedImageItem } from "./keyed-item";
 import { ILocation } from "./location";
 
+export interface IKPIEntry {
+    title: string,
+    titleSuffix?: string,
+    subtitle: string,
+    subtitleSuffix?: string,
+    linkPath?: string,
+}
+export interface IKPIViewModel {
+    updatedDate: number,
+    entries: Array<IKPIEntry>,
+}
+
 export interface IGridViewModel extends IKeyedImageItem {
     // id
     // name
