783cf39cd527550b3adb719fbec8126b8b65c86c
Paul Winfield <paul@Pauls-MacBook-Pro.local>
2020-10-18 10:56:28 +0100

    Added additional support class for route calculation

--- src/model/location.ts
+++ src/model/location.ts
@@ -1,4 +1,4 @@
-import { IAsset, IBreadcrumbPathEntry, IItemAttributeMap, ILocation, IPlottableLocation } from "../interfaces";
+import { IAsset, IBreadcrumbPathEntry, IItemAttributeMap, ILocation, IPlottableLocation, ISimpleTravelRoute } from "../interfaces";
 
 
 export class PlottableLocation implements IPlottableLocation {
@@ -24,6 +24,11 @@ export class PlottableLocation implements IPlottableLocation {
         }
         return `${this.lat},${this.lng}`;
     }    
+
+    public toGeoString(): string | undefined {
+        const result = this.toString();
+        return result ? `geo!${result}` : undefined;
+    }
 }
 
 
@@ -105,4 +110,45 @@ export class Location implements ILocation {
 
         return lines.length > 0 ? lines.join(', ') : '';
     }
+}
+
+
+export class SimpleTravelRoute implements ISimpleTravelRoute {
+    distanceInMetres: number;
+    timeInSeconds: number;
+    costFactor?: number;
+
+    constructor(route: ISimpleTravelRoute) {
+        this.distanceInMetres = route.distanceInMetres;
+        this.timeInSeconds = route.timeInSeconds;
+        this.costFactor = route.costFactor;
+    }
+
+    public get distanceInMiles(): number | undefined {
+        if (!this.distanceInMetres) {
+            return undefined;
+        }
+        let miles = this.distanceInMetres * 0.000621371;
+        return parseFloat(miles > 4 ? miles.toFixed(0) : miles.toFixed(1));
+    }
+
+    public get toTimeString(): string | undefined {
+        if (!this.timeInSeconds) {
+            return undefined;
+        }
+
+        const m = ~~((this.timeInSeconds % 3600) / 60);
+        if (this.timeInSeconds > 3600) {
+            let h = ~~(this.timeInSeconds / 3600);
+            return `${h} h ${m} min`;
+
+        }
+        return (this.timeInSeconds > 300) ? `${m} min` : '5 min';
+    }
+
+    public toString(): string | undefined {
+        const d = this.distanceInMiles;
+        const t = this.toTimeString;
+        return (!d || !t) ? '' : `${t}; ${d}`;
+    }    
 }
