9a48fb71b76dc7578d8a7517c8351c5f3ccdc369
Paul Winfield <paul@Pauls-MacBook-Pro.local>
2020-10-26 12:09:35 +0000

    Added supplier service mapping

--- src/interfaces/supplier.ts
+++ src/interfaces/supplier.ts
@@ -1,5 +1,8 @@
 import { IKeyedImageItem, ILocation, IContact } from ".";
 
+export interface ISupplierService extends IKeyedImageItem {
+    displayIndex?: number,
+}
 export interface ISupplier extends IKeyedImageItem {
     locations?: Array<ILocation>,
     contacts?: Array<IContact>,
@@ -8,4 +11,5 @@ export interface ISupplier extends IKeyedImageItem {
     quoteToCostRatio?: number,
     rating?: number,
     invite: boolean,
+    services?: Array<ISupplierService>,
 }
