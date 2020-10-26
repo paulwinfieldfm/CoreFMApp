22e3c6c5da88cad87d47df7b8885c08d055a5947
Paul Winfield <paul@Pauls-MacBook-Pro.local>
2020-10-26 09:56:09 +0000

    Added preference to supplier interface

--- src/interfaces/supplier.ts
+++ src/interfaces/supplier.ts
@@ -3,6 +3,7 @@ import { IKeyedImageItem, ILocation, IContact } from ".";
 export interface ISupplier extends IKeyedImageItem {
     locations?: Array<ILocation>,
     contacts?: Array<IContact>,
+    preferred?: boolean,
     avgQuoteResponse?: number,
     quoteToCostRatio?: number,
     rating?: number,
