497d0ebd7c98e27797c14cf2b4b52562eb739b79
Paul Winfield <paul@Pauls-MacBook-Pro.local>
2020-10-27 10:43:34 +0000

    New metrics on supplier interface

--- src/interfaces/supplier.ts
+++ src/interfaces/supplier.ts
@@ -1,6 +1,8 @@
 import { IKeyedImageItem, ILocation, IContact } from ".";
 
 export interface ISupplierService extends IKeyedImageItem {
+    active: boolean,
+    preferred: boolean,
     displayIndex?: number,
 }
 export interface ISupplierLocation extends ILocation {
@@ -11,6 +13,8 @@ export interface ISupplier extends IKeyedImageItem {
     contacts?: Array<IContact>,
     preferred?: boolean,
     url?: string,
+    ecoScore?: number,
+    firstFixRatio?: number,
     avgQuoteResponse?: number,
     quoteToCostRatio?: number,
     rating?: number,
