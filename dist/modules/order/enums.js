fd986a0f9678f1f295c5d939f7f0d654988b7f22
Paul Winfield <paul@Pauls-MacBook-Pro.local>
2021-06-07 12:27:40 +0100

    Added enum for order sign off status

---
+++ dist/modules/order/enums.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.OrderSignOffStatusType = void 0;
+var OrderSignOffStatusType;
+(function (OrderSignOffStatusType) {
+    OrderSignOffStatusType[OrderSignOffStatusType["signOffPending"] = 1] = "signOffPending";
+    OrderSignOffStatusType[OrderSignOffStatusType["signedOff"] = 2] = "signedOff";
+    OrderSignOffStatusType[OrderSignOffStatusType["queryRaised"] = 3] = "queryRaised";
+})(OrderSignOffStatusType = exports.OrderSignOffStatusType || (exports.OrderSignOffStatusType = {}));
+//# sourceMappingURL=enums.js.map
---
+++ src/modules/order/enums.ts
@@ -0,0 +1,5 @@
+export enum OrderSignOffStatusType {
+    signOffPending = 1,
+    signedOff,
+    queryRaised
+}
